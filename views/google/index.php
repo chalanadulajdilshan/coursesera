f95f1cfafba142e2e1723aee76f16723f01b0c82
DhanushaSuharshana929 <dhanusha@sublime.lk>
2021-03-01 15:04:30 +0530

    #91 Change Logo

--- views/google/index.php
+++ views/google/index.php
@@ -1,7 +1,7 @@
 <!-- Start main-content -->
 <div class="main-content bg-lighter">
     <!-- Section: inner-header -->
-    <section class="inner-header divider parallax layer-overlay overlay-dark-5" data-bg-img="<?php echo URL; ?>public/images/bg/bg6.jpg">
+    <section class="inner-header divider  layer-overlay overlay-dark-5" data-bg-img="<?php echo URL; ?>public/images/bg/bg6.jpg">
         <div class="container pt-70 pb-20">
             <!-- Section Content -->
             <div class="section-content">
@@ -22,9 +22,9 @@
     <section class="divider">
         <div class="container">
             <div class="row pt-30">
-    
+
                 <div class="col-md-12">
-                   <iframe width="100%" height="50" src="https://www.youtube.com/embed/iVOWiwPrCK0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="100%" height="50" src="https://www.youtube.com/embed/iVOWiwPrCK0?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
