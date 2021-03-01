3edb12f75ad0803755b861f7fbb6d990a660ee9a
DhanushaSuharshana929 <dhanusha@sublime.lk>
2021-03-01 12:34:38 +0530

    #87 Google Link

---
+++ views/google/index.php
@@ -0,0 +1,34 @@
+<!-- Start main-content -->
+<div class="main-content bg-lighter">
+    <!-- Section: inner-header -->
+    <section class="inner-header divider parallax layer-overlay overlay-dark-5" data-bg-img="<?php echo URL; ?>public/images/bg/bg6.jpg">
+        <div class="container pt-70 pb-20">
+            <!-- Section Content -->
+            <div class="section-content">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="title text-white text-center">Google</h2>
+                        <ol class="breadcrumb text-left text-black mt-10">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Pages</a></li>
+                            <li class="active text-gray-silver">Google</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Divider: Contact -->
+    <section class="divider">
+        <div class="container">
+            <div class="row pt-30">
+    
+                <div class="col-md-12">
+                   <iframe width="100%" height="50" src="https://www.youtube.com/embed/iVOWiwPrCK0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</div>
+<!-- end main-content -->
