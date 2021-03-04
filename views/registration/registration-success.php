e10f2c374f62f4b780c988563252675e0c9deba6
DhanushaSuharshana929 <dhanusha@sublime.lk>
2021-03-04 10:34:24 +0530

    #150 Success Message and slider text

--- views/registration/registration-success.php
+++ views/registration/registration-success.php
@@ -1,8 +1,6 @@
  
 <div id="wrapper" class="clearfix">
     <!-- preloader -->
-
-
     <!-- Start main-content -->
     <div class="main-content">
         <!-- Section: home -->
@@ -14,17 +12,16 @@
                             <div class="row">
                                 <div class="col-md-6 col-md-push-3">  
                                     <div class="  border-1px " id="first_form" style="padding: 25px 25px 25px 25px !important;background-color: #fff !important;"  >
-                                        <h3 class="mt-0 pt-5 text-center text-success font-weight-700" style="font-size: 32px;">SUCCESSFULLY..!</h3>
+                                        <h3 class="mt-0 pt-5 text-center text-success font-weight-700" style="font-size: 22px;">We received your registration.</h3>
+                                        <h3 class="mt-0 pt-5 text-center text-success font-weight-700" style="font-size: 22px;">Thank you very much !!</h3>
                                         <center>
                                             <img src="<?php echo MAIN_PUBLIC_URL ?>images/icon.jpg" alt=""/>
                                         </center>
-                                        <p>If you have any questions please contact with us Email</p>
-                                        <p><b>Thank you very much for your collaboration..! </b></p>
+                                        <p> If you have any questions or need clarifications please contact us help. <b><a href="mailto:skillsonline@mail.natlib.lk ">skillsonline@mail.natlib.lk </a></b></p>
                                         <hr> 
                                         <center>
                                             <a href="./../" type="button" class="btn btn-success">GO TO HOME</a>
                                         </center>
-
                                     </div>
                                 </div>
                             </div> 
